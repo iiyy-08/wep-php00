62221f8626205560595192f5d7e24c04da86cfd5
iiyy-08 <dlsdud3434@gmail.com>
2025-12-15 15:57:24 +0900

    Update check.php

--- PHP_project/check.php
+++ PHP_project/check.php
@@ -24,24 +24,25 @@
 
 	for ($i = 0; $i < count($word_ids); $i++) {
     // 문제 정답 조회
-    $qry = "SELECT kr_mean FROM words WHERE word_id=".$word_ids[$i];
-    $rst = mysqli_query($conn, $qry);
-    $row = mysqli_fetch_assoc($rst);
-
-    $is_correct = (trim($answers[$i]) == trim($row['kr_mean'])) ? 1 : 0;
-
-    // 점수 합산
-    if ($is_correct) $score++;
-
-    // DB에 기록
-    $qry_insert = "INSERT INTO records (user_id, word_id, is_correct) VALUES ('".$_SESSION['user_id']."', ".$word_ids[$i].", $is_correct)";
-    mysqli_query($conn, $qry_insert);
+	    $qry = "SELECT kr_mean FROM words WHERE word_id=".$word_ids[$i];
+	    $rst = mysqli_query($conn, $qry);
+	    $row = mysqli_fetch_assoc($rst);
 	
-	$_SESSION['score'] = $score;
-}
+	    $is_correct = (trim($answers[$i]) == trim($row['kr_mean'])) ? 1 : 0;
+	
+	    // 점수 합산
+	    if ($is_correct) $score++;
+	
+	    // DB에 기록
+	    $qry_insert = "INSERT INTO records (user_id, word_id, is_correct) VALUES ('".$_SESSION['user_id']."', ".$word_ids[$i].", $is_correct)";
+	    mysqli_query($conn, $qry_insert);
+		
+		$_SESSION['score'] = $score;
+	}
 
 	echo "<script>";
 	echo "alert('문제 끝');";
 	echo "location.href='result.php';";
 	echo "</script>";
+
 ?>
